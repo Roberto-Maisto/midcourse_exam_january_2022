4c8f240e38d82a9b4a7556e9d4402cfdca0ee0a1
Roberto <maisto_roberto@hotmail.it>
2022-03-10 12:18:56 +0100

    Failed steps

--- cypress/integration/displayUsersList.feature.js
+++ cypress/integration/displayUsersList.feature.js
@@ -4,5 +4,12 @@ beforeEach(() => {
 })
     it('displays a header', () => {
         cy.get('#header').shoulder('contain', 'Users list ')
-    });
-})
+        it('displays a list with 6 users', () => {
+            cy.get('#users-list').within(() => {
+                cy.get('.users-list')
+                    .first()
+                    .find('.name')
+                .should('contain', 'Pinco Pallino')
+            });
+        });
+    }
